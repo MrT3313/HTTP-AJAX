99f75dcca54b63898c9caba03873a7eb143b04c3
MrT3313 <rturgeon13@gmail.com>
2019-04-02 10:34:22 -0500

    user input updating state on <NewFriendForm />

--- friends/src/Components/NewFriendForm.js
+++ friends/src/Components/NewFriendForm.js
@@ -6,20 +6,50 @@ export default class NewFriendForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            friends: []
+            input_firstName: '',
+            input_lastName: '',
+            input_age: '',
+            input_email: ''
         }
     }
 
+    changeHandler_firstName = e => {
+        this.setState( { input_firstName: e.target.value } )
+    }
+    changeHandler_lastName = e => {
+        this.setState( { input_lastName: e.target.value } )
+    }
+    changeHandler_age = e => {
+        this.setState( { input_age: e.target.value } )
+    }
+    changeHandler_email = e => {
+        this.setState( { input_email: e.target.value } )
+    }
 
     render() {
         return (
             <div>
                 <h2>New Friend Entry Form</h2>
                 <form>
-                    <input type='text'      name='FirstName'    placeholder='First Name'  ></input>
-                    <input type='text'      name='LastName'     placeholder='Last Name'   ></input>
-                    <input type='number'    name='LastName'     placeholder='Age'         ></input>
-                    <input type='text'      name='email'        placeholder='Email'       ></input>
+                    <input type='text'      
+                        name='FirstName'    placeholder='First Name'  
+                        onChange={this.changeHandler_firstName}
+                    ></input>
+
+                    <input type='text'      
+                        name='LastName'     placeholder='Last Name' 
+                        onChange={this.changeHandler_lastName}  
+                    ></input>
+
+                    <input type='number'    
+                        name='LastName'     placeholder='Age'   
+                        onChange={this.changeHandler_age}      
+                    ></input>
+
+                    <input type='text'      
+                        name='email'        placeholder='Email'  
+                        onChange={this.changeHandler_email}     
+                    ></input>
                 </form>
             </div>
         )
