dc8a3b402ca5944e1d2ca42990ff8795002e02bb
MrT3313 <rturgeon13@gmail.com>
2019-04-05 21:46:24 -0500

    STUCK -- cant get calue out of material UI FAB to update state

--- friends/src/components/form.js
+++ friends/src/components/form.js
@@ -19,33 +19,47 @@ const Styled_form = styled.form`
 export default class Form extends Component {
     constructor(props) {
         super(props)
-
+        this.state = {
+            name: undefined,
+            age: undefined,
+            email: undefined
+        }
 
     }
+
 
     render() {
         return (
                 <>
                 <Styled_form>
                     <TextField
+                        id="name"
+                        type='text'
+                        
                         label="Name"
-                        type='text'
+                        onChange={this.inputChangeHandler}
                         // value={this.state.name}
                         // onChange={this.handleChange('name')}
                         margin="normal"
                         variant="outlined"
                     />
                     <TextField
-                        label="Age"
                         type="number"
+                        id="name"
+
+                        label="Age"
+                        onChange={this.inputChangeHandler}
                         // value={this.state.name}
                         // onChange={this.handleChange('name')}
                         margin="normal"
                         variant="outlined"
                     />
                     <TextField
-                        label="email"
                         type='text'
+                        id="name"
+
+                        label="email"
+                        onChange={this.inputChangeHandler}
                         // value={this.state.name}
                         // onChange={this.handleChange('name')}
                         margin="normal"
